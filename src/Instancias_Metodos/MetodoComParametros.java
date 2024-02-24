c9a0ad2e69c1a06a63eeb9e63d5c4f235cd8354c
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-02-24 12:32:32 -0300

    'CCorrecção do Exercicios Metodos com e sem Parametros

--- src/Instancias_Metodos/MetodoComParametros.java
+++ src/Instancias_Metodos/MetodoComParametros.java
@@ -5,33 +5,64 @@ import com.sun.tools.javac.Main;
 public class MetodoComParametros {
 
 
- public  static void metodoComRetorno(String nome){
-   System.out.println(nome);
+  public static void metodoUmSemRetorno(String nome) {
+      System.out.println(nome);
 
- }
-   // public  static void metodoComRetorno(int idade){
-    //    System.out.println(idade);
+  }
+      public static String metodoUmComRetorno (String nome){
+          return " O parametro enviado é " + nome;
+      }
+      public static void metodoDoisSemRetorno (String nome){
+          System.out.println(nome);
 
-    }
+      }
+      public static String metodoDoisComRetorno (String nome){
 
-  //  public  static void metodoComRetorno(int altura){
-     //   System.out.println(altura);
-
-   // }
-
-  // public  static void String metodoComRetorno(String nome){
-    //   {
-  //   return  " o parametro enviado é "+ nome;
-//}
+      return " O parametro enviado é " + nome;
+      }
 
 
-    //   public static void main(String[] args) {
-    //       metodoComRetorno("Adriana");
-   //    }
+      public static void metodoTresSemRetorno (String nome){
+          System.out.println(nome);
+
+      }
+      public static String metodoTresComRetorno (String nome)
+      {
+          return " O parametro enviado é " + nome;
+      }
+
+      public static void metodoQuatroSemRetorno (String nome){
+          System.out.println(nome);
+
+      }
+      public static String metodoQuatroComRetorno (String nome)      {
+          return " O parametro enviado é " + nome;
+      }
 
 
-// }
-//}
+      public static void metodoCincoSemRetorno (String nome){
+          System.out.println(nome);
 
+      }
+      public static String metodoCincoComRetorno (String nome){
+          return " O parametro enviado é " + nome;
+      }
+
+
+      public static void main (String[]args){
+          System.out.println(metodoUmComRetorno("Adriana"));
+          System.out.println(metodoDoisComRetorno("Adnys"));
+          System.out.println(metodoTresComRetorno("Flavio"));
+          System.out.println(metodoQuatroComRetorno("Jesse"));
+          System.out.println(metodoCincoComRetorno("Valeria"));
+
+      metodoUmSemRetorno("Adriana -1");
+      metodoDoisSemRetorno("Adnys -2");
+      metodoTresSemRetorno("Flavio - 3");
+      metodoQuatroSemRetorno("Jesse - 4 ");
+      metodoCincoSemRetorno("Valeria - 5");
+      }
+
+  }
 
 
