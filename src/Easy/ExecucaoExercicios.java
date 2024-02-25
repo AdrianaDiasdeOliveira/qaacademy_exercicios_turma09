9fe939dd6a668754b1cd452bf5a618a4cb97775e
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-02-25 15:26:42 -0300

    src/Easy/ExecucaoExercicios.java

--- src/Easy/ExecucaoExercicios.java
+++ src/Easy/ExecucaoExercicios.java
@@ -17,11 +17,34 @@ public class ExecucaoExercicios {
      Exercicio_03_easy exercicio03Easy = new Exercicio_03_easy();
      System.out.println(exercicio03Easy.armazenarValoresTrocados(valor1 , valor2));*/
 
-        //Exercicio_04_easy
-        String numeroDigitado = JOptionPane.showInputDialog("Digite o Número Inteiro:");
+
+                //Exercicio_04_easy
+/*      String numeroDigitado = JOptionPane.showInputDialog("Digite o Número Inteiro:");
         int  numero1 = Integer.parseInt(numeroDigitado); // Conversão e atribuição na variavel numeroDigitado
         Exercicio_04_Licao_de_casa exercicio04Easy = new Exercicio_04_Licao_de_casa();
-        System.out.println("O dobro do valor de: " + numero1 + " é " + exercicio04Easy.calcularDobro(numero1));
+        System.out.println("O dobro do valor de: " + numero1 + " é " + exercicio04Easy.calcularDobro(numero1));*/
+
+
+//Exercicio_05_easy
+
+        String primeiroNumero = JOptionPane.showInputDialog("Digite o Primeiro Número: ");
+        String segundoNumero = JOptionPane.showInputDialog("Digite o Segundo  Número: ");
+        String terceiroNumero = JOptionPane.showInputDialog("Digite o Terceiro Número: ");
+
+        int num1 = Integer.parseInt(primeiroNumero);//Conversão da variavel primeiroNumero / segundoNumero /terceiroNumero para Inteiro.
+        int num2 = Integer.parseInt(segundoNumero);
+        int num3 = Integer.parseInt(terceiroNumero);
+
+        Exercicio_05_Licao_de_casa exercicio_05_easy = new Exercicio_05_Licao_de_casa();
+        System.out.println( "A soma dos numeros Digitados será: " + exercicio_05_easy.exibirSoma(num1,num2,num3));
+        System.out.println( "A Subtração dos numeros Digitados será: " + exercicio_05_easy.exibirSubitracao(num1,num2));
+        System.out.println( "A Multiplicação dos numeros Digitados será: " + exercicio_05_easy.exibirMultiplicacao(num1,num2,num3));
+        System.out.println( "A Media dos numeros Digitados será: " + exercicio_05_easy.exibirMedia(num1,num2,num3));
+
+
+
+
+
 
 
 
--- src/Easy/Exercicio_05_Licao_de_casa.java
+++ src/Easy/Exercicio_05_Licao_de_casa.java
@@ -2,28 +2,40 @@ package Easy;
 
 import javax.swing.*;
 import java.sql.SQLOutput;
-
-public class Exercicio_05_Licao_de_casa {
-    public static void main(String[] args) {
-
 // 5-) Faça um algoritmo para ler três números inteiros, armazenar em variáveis,e exibir a soma, a subtração, multiplicação e a média dos três números digitados.
 
-        Integer num1, num2, num3, media;
+public class Exercicio_05_Licao_de_casa {
+    public int exibirSoma (int num1 ,int num2 , int num3 ) {
 
-        String primeiroNumero = JOptionPane.showInputDialog("Digite o Primeiro Número: ");
-        String segundoNumero = JOptionPane.showInputDialog("Digite o Segundo  Número: ");
-        String terceiroNumero = JOptionPane.showInputDialog("Digite o Terceiro Número: ");
+        Integer media;
 
-            //Conversão da variavel primeiroNumero / segundoNumero /terceiroNumero para Inteiro.
+        // String primeiroNumero = JOptionPane.showInputDialog("Digite o Primeiro Número: ");
+        // String segundoNumero = JOptionPane.showInputDialog("Digite o Segundo  Número: ");
+        // String terceiroNumero = JOptionPane.showInputDialog("Digite o Terceiro Número: ");
 
-        num1 = Integer.parseInt(primeiroNumero);
-        num2 = Integer.parseInt(segundoNumero);
-        num3 = Integer.parseInt(terceiroNumero);
+        //Conversão da variavel primeiroNumero / segundoNumero /terceiroNumero para Inteiro.
 
-        System.out.println("A Soma é:" + (num1 + num2 + num3));
-        System.out.println(" A subtracao é:" + (num1 - num2 ));
-        System.out.println("A multiplicacao é:" + (num1 * num2 * num3));
-        System.out.println("A média dos três números digitados :" + (num1 + num2 + num3)/3);
+        //  num1 = Integer.parseInt(primeiroNumero);
+        //  num2 = Integer.parseInt(segundoNumero);
+        //  num3 = Integer.parseInt(terceiroNumero);
 
-       }
+        return num1 + num2 + num3;
+
+    }
+        public int exibirSubitracao (int num1 ,int num2 ) {
+
+            return num1 - num2;
+
+        }
+
+    public int exibirMultiplicacao (int num1 ,int num2 , int num3 ) {
+        return num1 * num2 * num3;
+
+    }
+
+    public int exibirMedia (int num1 ,int num2 , int num3 ) {
+        return (num1 + num2 + num3 )/ 3;
+
+    }
 }
+
