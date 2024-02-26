27bdac6516e865ed706129246fae2e2e9cf4aac9
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-02-26 20:23:55 -0300

    Refatoração do ex: 14 nivel medium

--- src/Medium/Exercicio_14_Licao_de_casa_Nivel_Medium.java
+++ src/Medium/Exercicio_14_Licao_de_casa_Nivel_Medium.java
@@ -1,7 +1,7 @@
 package Medium;
 
 public class Exercicio_14_Licao_de_casa_Nivel_Medium {
-    public static void main(String[] args) {
+    public  void ExibirTodasSomas () {
 
         //3-) Faça um algoritmo para calcular e exibir a soma de todos os números
         //de 0 a 1000. E parar se caso a soma atingir 1500(Ou mais).
--- src/Medium/RefatoracaoMedium.java
+++ src/Medium/RefatoracaoMedium.java
@@ -6,17 +6,22 @@ public class RefatoracaoMedium {
 
 
         // Exercicio_11_Licao_de_casa_Nivel_Medium
-/*         Integer numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um Número:"));
+/*      Integer numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um Número:"));
         Exercicio_11_Licao_de_casa_Nivel_Medium exercicio_11_Nivel_Medium = new Exercicio_11_Licao_de_casa_Nivel_Medium();
         System.out.println(exercicio_11_Nivel_Medium.LerInteiro(numeroDigitado));*/
 
 
         //  Exercicio_12_Licao_de_casa_Nivel_Medium
 
-        Exercicio_12_Licao_de_casa_Nivel_Medium exercicio_12_Licao_de_casa = new Exercicio_12_Licao_de_casa_Nivel_Medium();
+ /*     Exercicio_12_Licao_de_casa_Nivel_Medium exercicio_12_Licao_de_casa = new Exercicio_12_Licao_de_casa_Nivel_Medium();
         exercicio_12_Licao_de_casa.verificarParesEImpar();
+*/
 
 
+         // Exercicio_14_Licao_de_casa_Nivel_Medium
+
+        Exercicio_14_Licao_de_casa_Nivel_Medium  exercicio_14_Nivel_Medium = new Exercicio_14_Licao_de_casa_Nivel_Medium();
+        exercicio_14_Nivel_Medium.ExibirTodasSomas();
 
 
 
