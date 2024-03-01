912fe5227ca38673c4d7a94c53b24fb0d9ef8fd7
AdrianaDiasdeOliveira <adriana.dias3003@gmail.com>
2024-03-01 19:40:38 -0300

    refatorar o exercicio 06

--- src/Vetores/Exercicio_06_Licao_de_casa_Nivel_Medium.java
+++ src/Vetores/Exercicio_06_Licao_de_casa_Nivel_Medium.java
@@ -3,25 +3,26 @@ package Vetores;
 import javax.swing.*;
 
 public class Exercicio_06_Licao_de_casa_Nivel_Medium {
-    public  void ExibirFatorial() {
+    public  int  ExibirFatorial( int numeroFatorial) {
 
         // 6-) Faça um algoritmo para ler um número inteiro, calcular e exibir o
         //  Fatorial do número digitado. Ex.: Digitou 10. 10*(10-1)*(10-2)*(10-3)..*(10-1) = 3.628.800
 //  Fatorial, i;    //  Fatorial = Fatorial * (i-1);
 
-        int fatorial = 1;
+      int fatorial = 1;
 
-        Integer numeroFatorial = Integer.parseInt(JOptionPane.showInputDialog("Informe um Número para calcular o Fatorial: "));
+      //  Integer numeroFatorial = Integer.parseInt(JOptionPane.showInputDialog("Informe um Número para calcular o Fatorial: "));
 
         for (int i = numeroFatorial; i > 1; i--) {
             numeroFatorial = numeroFatorial * (i-1);
         }
         if (numeroFatorial == 0) {
-            System.out.println("O Fatorial do numero digitado é : 1");
+          //  System.out.println("O Fatorial do numero digitado é : 1");
         }else{
            System.out.println("O Fatorial do numero digitado é  :" + numeroFatorial);
 
         }
+        return fatorial;
     }
 }
 
--- src/Vetores/RefatoracaoDosExercicios.java
+++ src/Vetores/RefatoracaoDosExercicios.java
@@ -5,30 +5,33 @@ import javax.swing.*;
 public class RefatoracaoDosExercicios {
     public static void main(String[] args) {
 
-        // Exercicio_05_Licao_de_casa_Nivel_Medium
+        // Exercicio_05
+
 //       Exercicio_05_Licao_de_casa_Nivel_Medium  exercicio_05_NivelMedium = new Exercicio_05_Licao_de_casa_Nivel_Medium();
 //      exercicio_05_NivelMedium.VerificarNumeros();
 
-       //  Exercicio_06_Licao_de_casa_Nivel_Medium
-//        Exercicio_06_Licao_de_casa_Nivel_Medium  exercicio_06_Medium = new Exercicio_06_Licao_de_casa_Nivel_Medium();
-//        exercicio_06_Medium.ExibirFatorial();
+       //  Exercicio_06
+
+    //    Integer numeroFatorial = Integer.parseInt(JOptionPane.showInputDialog("Informe um Número para calcular o Fatorial: "));
+    //    Exercicio_06_Licao_de_casa_Nivel_Medium  exercicio_06_Medium = new Exercicio_06_Licao_de_casa_Nivel_Medium();
+     //   numeroFatorial = exercicio_06_Medium.ExibirFatorial(numeroFatorial);
+     //  System.out.println("O Fatorial do numero digitado é : 1");
 
 
-       //  Exercicio_07_Licao_de_casa_Nivel_Medium
+
+        //  Exercicio_07_Licao_de_casa_Nivel_Medium
 //        Exercicio_07_Licao_de_casa_Nivel_Medium  exercicio_07_Medium = new Exercicio_07_Licao_de_casa_Nivel_Medium();
 //        exercicio_07_Medium.ExibirMeses();
 
         //  Exercicio_08_Licao_de_casa_Nivel_Medium
-//       Exercicio_08_Licao_de_casa_Nivel_Medium  exercicio_08_Medium = new Exercicio_08_Licao_de_casa_Nivel_Medium();
-//       exercicio_08_Medium.LerMeses();
-
-
+      Exercicio_08_Licao_de_casa_Nivel_Medium  exercicio_08_Medium = new Exercicio_08_Licao_de_casa_Nivel_Medium();
+      exercicio_08_Medium.LerMeses();
 
 
 
         //  Exercicio_10_Licao_de_casa_Nivel_Medium
-   Exercicio_10_Licao_de_casa_Nivel_Medium  exercicio_10_Medium = new Exercicio_10_Licao_de_casa_Nivel_Medium();
-    exercicio_10_Medium.ExibirSequencias();
+  // Exercicio_10_Licao_de_casa_Nivel_Medium  exercicio_10_Medium = new Exercicio_10_Licao_de_casa_Nivel_Medium();
+  //  exercicio_10_Medium.ExibirSequencias();
 
 
     }
